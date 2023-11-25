c34c728c3af3eabb508139c8fa445b45b4672128
Sailendra Darbha <sailendra.darbha@gmail.com>
2023-11-25 16:35:04 +0530

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         </div>
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700">
-            Amplify Dentistry is now in public beta!
+            AmplifyDentistry is now in public beta!
           </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
