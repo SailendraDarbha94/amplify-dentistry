9ec3bc513fe1547f93de0a0fb04a7f2aa2e7d24c
Sailendra Darbha <sailendra.darbha@gmail.com>
2023-11-30 13:51:13 +0530

    bugfixes

--- src/app/years/fourth/page.tsx
+++ src/app/years/fourth/page.tsx
@@ -2,14 +2,13 @@
 import { trpc } from "@/app/_trpc/client";
 import Skeleton from "react-loading-skeleton";
 import Book from "@/components/Book";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+// import { redirect } from "next/navigation";
 
 const Page = () => {
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
-
-  if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  // const { getUser } = getKindeServerSession()
+  // const user = getUser()
+  // if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
   const { data: allFiles, isLoading } = trpc.getFourthYearBooks.useQuery()
   return (
     <main className="mx-auto max-w-7xl md:p-10">
