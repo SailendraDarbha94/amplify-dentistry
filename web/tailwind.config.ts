32e954a2faa76ced4e57f155ffb7f7f613536a3d
SailendraDarbha94 <sailendra.darbha@gmail.com>
2024-05-08 17:18:36 +0530

    fresh repo with poppins font

---
+++ web/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      pLight: ["pLight", "sans-serif"],
+      pMedium: ["pMedium", "sans-serif"],
+      pBold: ["pBold", "sans-serif"],
+      pExtraBold: ["pExtraBold", "sans-serif"]
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
